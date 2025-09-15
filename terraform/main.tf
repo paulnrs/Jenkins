4a8deb6159f3f86202d253e2c454a6e73bc0d7cd
Paul Rousseau <93522786+paulnrs@users.noreply.github.com>
2025-09-15 12:03:31 +0200

    Add AWS provider and EC2 instance resource

---
+++ terraform/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  skip_credentials_validation = true
+  endpoints {
+    ec2 = "http://ip10-0-34-6-d33s8lpntdlhbpdsdc6g-8080.direct.lab-boris.fr"
+  }
+}
+
+resource "aws_instance" "demo" {
+  ami           = "ami-12345678"
+  instance_type = "t2.micro"
+}
